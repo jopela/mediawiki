3a4611a02dd6f5d8e1ae9260ea48d045d1190702
Jonathan Pelletier <jonathan.pelletier1@gmail.com>
2014-05-05 12:54:24 -0400

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mediawiki
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ src/mediawiki/requests.clj
@@ -0,0 +1,55 @@
+(ns mediawiki.requests)
+
+(defn geocoords
+  "returns the geocoordinates of a page. Returns nil if the page has
+  no coordinates."
+  [page]
+  (let [params {}]
+  [45 -73])
+
+(defn language-links
+  "returns all the language links of a given mediawiki page (including the
+  language of the queried wiki). Languages of the pages can be inferred by the 
+  url." 
+  [page]
+  ["http://en.wikipedia.org/Montreal" "http://fr.wikipedia.org/Montreal"])
+
+(defn image-links
+  "returns all the images url of a given page."
+  [page]
+  ["http://en.wikipedia.org/photo/omg.jpg" "http://en.wikipedia.org/p/ok.jpg"])
+
+(defn introduction-html
+  "returns the introductory text of a page (abstract). The text format is
+  html."
+  [page]
+  "<p><b>Montreal is a great city</b></p>")
+
+(defn article-html
+  "returns the article of the page in html format."
+  [page]
+  "<h2>History</h2><p> The history of Montreal is fun</p>")
+
+(defn depiction
+  "returns the url of the image that acts as the depiction of the page"
+  [page]
+  "http://en.wikipedia.org/wiki/picture.jpeg")
+
+(defn categories
+  "return the categories to which the given page belongs."
+  [page]
+  (into #{} ["Populated places in 1642" "Montreal"]))
+
+(defn external-links
+  "return the external urls from the given page. For inter-wiki links,
+  used the inter-wiki-links function."
+  [page]
+  (into #{} ["http://stm.info" "http://meteo-montreal.com"]))
+
+(defn inter-wiki-links
+  "return url links to other wiki's (wikivoyage, wikibooks etc) for the given
+  page."
+  [page]
+  (into #{} ["http://en.wikivoyage.org/wiki/Montreal" 
+             "http://en.wikibooks.org/wiki/Montreal"]))
+
